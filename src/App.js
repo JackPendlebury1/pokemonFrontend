43f617a34bdebc5e94450192d228def40355c448
NeinomasWin <neinomas@gmail.com>
2022-01-18 13:28:06 +0000

    useeffect broken, failed to deploy, fixes

--- src/App.js
+++ src/App.js
@@ -8,13 +8,16 @@ import Cookies from 'js-cookie';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
-  //comment
+ 
   useEffect(() => {
+    const fetchData = async () => {
     const response = await fetch(`${process.env.REACT_APP_ENDPOINT}profile`,
             { method: 'POST', headers: { 'Content-Type': 'application/json', "Authorization": Cookies.get("login") } });
         if (response.ok) {
           setIsLoggedIn(true)
         }
+      }
+      fetchData()
   },[])
 
 
