6701a7eae57f56ec49001c719eecdc42963b198e
NeinomasWin <neinomas@gmail.com>
2022-04-08 20:45:07 +0100

    updates

--- src/components/useAuth.js
+++ src/components/useAuth.js
@@ -1,20 +1,20 @@
 class Auth {
     constructor() {
-        this.authenticated = false; 
+      this.authenticated = false;
     }
-
-    login(cb){
-        this.authenticated = true;
-        cb()
+  
+    login(cb) {
+      this.authenticated = true;
+      cb();
     }
-
-    logout(cb){
-        this.authenticated = false;
-        cb()
+  
+    logout(cb) {
+      this.authenticated = false;
+      cb();
     }
-
+  
     isAuthenticated() {
-        return this.authenticated;
+      return this.authenticated;
     }
-}
+  }
 export default new Auth();
